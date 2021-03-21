de8ae859e905c31649bebfe656c9a299eee71319
MoonJuhan <codemike@naver.com>
2021-03-21 17:38:17 +0900

    feat - 기본 기능 테스트 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+const array: Array<String> = ['data0'];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'Angular Study';
+  showArray: Array<String>;
+
+  // Vue.js 2.x의 Created 와 유사
+  constructor() {
+    console.log('Working');
+    this.showArray = array;
+  }
+
+  public clickAfterPrint(): void {
+    console.log('Btn');
+    this._innerFunc();
+    console.log(array);
+  }
+
+  private _innerFunc() {
+    array.push('data' + array.length);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NextComponentComponent } from './next-component/next-component.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NextComponentComponent, SecondCompComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
